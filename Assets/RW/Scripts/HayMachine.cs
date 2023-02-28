b281a5dc7f8eb47589f8385888cc8507d839a602
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2023-03-01 08:23:01 +1300

    Finished lecture-notes 2

--- Assets/RW/Scripts/HayMachine.cs
+++ Assets/RW/Scripts/HayMachine.cs
@@ -6,6 +6,10 @@ public class HayMachine : MonoBehaviour
 {
     public float movementSpeed;
     public float horizontalBoundary = 22;
+    public GameObject hayBalePrefab;
+    public Transform haySpawnpoint;
+    public float shootInterval;
+    private float shootTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,7 @@ public class HayMachine : MonoBehaviour
     void Update()
     {
         UpdateMovement();
+        UpdateShooting();
     }
 
     private void UpdateMovement()
@@ -24,5 +29,21 @@ public class HayMachine : MonoBehaviour
 
         if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) transform.Translate((transform.right * -movementSpeed) * Time.deltaTime);
         if (horizontalInput > 0&& transform.position.x < horizontalBoundary) transform.Translate((transform.right * movementSpeed) * Time.deltaTime);
+    }
+
+    private void UpdateShooting()
+{
+    shootTimer -= Time.deltaTime; 
+
+    if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) 
+        {
+            shootTimer = shootInterval; 
+            ShootHay(); 
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
     }
 }
