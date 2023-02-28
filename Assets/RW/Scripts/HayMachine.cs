5ae1ede74a0cd78ac7f28d897db053119c30ed5f
legin8 <maynne1@student.op.ac.nz>
2023-02-28 21:25:41 +1300

    Inital commit, class 1 work done. class 2 hay thing boundrys set

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) transform.Translate((transform.right * -movementSpeed) * Time.deltaTime);
+        if (horizontalInput > 0&& transform.position.x < horizontalBoundary) transform.Translate((transform.right * movementSpeed) * Time.deltaTime);
+    }
+}
